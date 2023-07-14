b5ea6f29c6f07622eb84478dd108123bcccd07c2
Raj Kumar Baliyar Singh <35659326+rajkbs@users.noreply.github.com>
2023-07-14 12:43:39 +0530

    Add files via upload

---
+++ intro_structure.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Structure Declaration
+
+struct student{
+    char name[25];
+    int roll_no;
+};
+
+int main(){
+    struct student s;
+
+    printf("Enter Student name : ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("\nEnter Roll Number : ");
+    scanf("%d", &s.roll_no);
+
+    printf("\n--Display--\n");
+
+    printf("%s\n", s.name);
+    printf("%d", s.roll_no);
+    return 0;
+}
